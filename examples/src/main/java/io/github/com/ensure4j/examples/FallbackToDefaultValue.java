5b0520245b0c7a5c3485370927aea9a4469f098e
Erik Olsson <olsson.erik1993@gmail.com>
2025-11-17 17:25:36 +0100

    add some more examples

--- examples/src/main/java/io/github/com/ensure4j/examples/FallbackToDefaultValue.java
+++ examples/src/main/java/io/github/com/ensure4j/examples/FallbackToDefaultValue.java
@@ -23,11 +23,6 @@ public class FallbackToDefaultValue {
                 .map(v -> Ensure.notNullOrElse(v, VehicleDetails.DEFAULT))
                 .forEach(System.out::println);
         // or create a default value on demand
-        l = new ArrayList<>();
-        l.add(null);
-        l.add(new VehicleDetails("V70", "Blue"));
-        l.add(null);
-        l.add(new VehicleDetails("XC90", "Blue"));
         l.stream()
                 .map(v -> Ensure.notNullOrElseGet(v, () -> new VehicleDetails("unknown", "unknown")))
                 .forEach(System.out::println);
