13e3ba0174e7cc8c08e97071df6ac9e013bb97b7
Erik Olsson <olsson.erik1993@gmail.com>
2025-11-18 16:14:13 +0100

    jspecify markers

--- lib/src/main/java/io/github/mangila/ensure4j/package-info.java
+++ lib/src/main/java/io/github/mangila/ensure4j/package-info.java
@@ -1,5 +1,12 @@
 /**
- * Make this package @NullUnmarked since we will be treating "null" alot in here
+ * <p>
+ * Annotated with {@link org.jspecify.annotations.NullUnmarked} since this package handles Unspecified nullness
+ * </p>
+ * Package contains:
+ * <ul>
+ *     <li>{@link io.github.mangila.ensure4j.Ensure} - The utility implementation</li>
+ *     <li>{@link io.github.mangila.ensure4j.EnsureException} - The exception that will be thrown when a precondition fails</li>
+ * </ul>
  */
 @NullUnmarked
 package io.github.mangila.ensure4j;
