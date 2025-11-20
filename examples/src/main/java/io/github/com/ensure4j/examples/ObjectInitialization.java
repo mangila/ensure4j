c5af48d8c1d82cdb0e45775a333c45e26538634e
Erik Olsson <84154996+mangila@users.noreply.github.com>
2025-11-20 01:09:27 +0100

    0.0.8 (#14)
    Got stuck in a infinite loop in circle ci forgot to use [skip ci] Wops!!
    
    * return values instead
    
    * return values instead
    
    * [maven-release-plugin] prepare release ensure4j-0.0.8
    
    * [maven-release-plugin] prepare for next development iteration
    
    * [maven-release-plugin] prepare release ensure4j-0.0.9
    
    * [maven-release-plugin] prepare for next development iteration
    
    * [maven-release-plugin] prepare release ensure4j-0.0.10
    
    * [maven-release-plugin] prepare for next development iteration
    
    * [maven-release-plugin] prepare release ensure4j-0.0.11
    
    * [maven-release-plugin] prepare for next development iteration
    
    * [maven-release-plugin] prepare release ensure4j-0.0.12
    
    * [maven-release-plugin] prepare for next development iteration
    
    * [maven-release-plugin] prepare release ensure4j-0.0.13
    
    * [maven-release-plugin] prepare for next development iteration
    
    * [maven-release-plugin] prepare release ensure4j-0.0.14
    
    * [maven-release-plugin] prepare for next development iteration
    
    * [maven-release-plugin] prepare release ensure4j-0.0.15
    
    * [maven-release-plugin] prepare for next development iteration
    
    * [maven-release-plugin] prepare release ensure4j-0.0.16
    
    * [maven-release-plugin] prepare for next development iteration
    
    * [maven-release-plugin] prepare release ensure4j-0.0.17
    
    * [maven-release-plugin] prepare for next development iteration
    
    * [maven-release-plugin] prepare release ensure4j-0.0.18
    
    * [maven-release-plugin] prepare for next development iteration
    
    * [maven-release-plugin] prepare release ensure4j-0.0.19
    
    * [maven-release-plugin] prepare for next development iteration
    
    * [maven-release-plugin] prepare release ensure4j-0.0.20
    
    * [maven-release-plugin] prepare for next development iteration
    
    * skip ci
    
    * fix infinite loop circleci

--- examples/src/main/java/io/github/com/ensure4j/examples/ObjectInitialization.java
+++ examples/src/main/java/io/github/com/ensure4j/examples/ObjectInitialization.java
@@ -7,13 +7,16 @@ public class ObjectInitialization {
     static class EnsuredObject {
         String key;
         String value;
+        int number;
 
         // Run some preconditions in the class constructor
-        EnsuredObject(String key, String value) {
+        EnsuredObject(String key, String value, int number) {
             Ensure.notBlank(key, () -> new IllegalArgumentException("key must not be blank"));
             Ensure.notBlank(value, () -> new IllegalArgumentException("value must not be blank"));
+            Ensure.min(1, number, () -> new IllegalArgumentException("number must be greater than or equal to 0"));
             this.key = key;
             this.value = value;
+            this.number = number;
         }
 
     }
@@ -21,38 +24,44 @@ public class ObjectInitialization {
     static class EnsuredObject1 {
         String key;
         String value;
+        int number;
 
         // Run some preconditions in the class constructor
         // and fallback with a default value if the precondition fails
         // Eager Initialization
-        EnsuredObject1(String key, String value) {
+        EnsuredObject1(String key, String value, int number) {
             this.key = Ensure.notNullOrElse(key, "default_key");
             this.value = Ensure.notNullOrElse(value, "default_value");
+            this.number = Ensure.min(1, number, "number must be greater than or equal to 0");
         }
     }
 
     static class EnsuredObject2 {
         String key;
         String value;
+        int number;
 
         // Run some preconditions in the class constructor
         // and fallback with a default value if the precondition fails
         // run with a Supplier instead. Lazy Initialization
-        EnsuredObject2(String key, String value) {
+        EnsuredObject2(String key, String value, int number) {
             this.key = Ensure.notNullOrElseGet(key, () -> "default_key");
             this.value = Ensure.notNullOrElseGet(value, () -> "default_value");
+            this.number = Ensure.min(1, number, () -> new IllegalArgumentException("number must be greater than or equal to 0"));
         }
     }
 
     static class EnsuredObject3 {
         String key;
         String value;
+        int number;
 
         // Run some preconditions in the class constructor
         // use the value, but only if it's not null
-        EnsuredObject3(String key, String value) {
+        EnsuredObject3(String key, String value, int number) {
             this.key = Ensure.notNullOrElseThrow(key);
             this.value = Ensure.notNullOrElseThrow(value);
+            this.number = Ensure.min(1, number, () -> new IllegalArgumentException("number must be greater than or equal to 0"));
         }
     }
 }
