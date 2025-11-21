4c297e68f4b0afadf4285470266f15e8d28113ea
Erik Olsson <olsson.erik1993@gmail.com>
2025-11-21 11:37:17 +0100

    rename equals -> isEquals, rename some params

--- lib/src/test/java/io/github/mangila/ensure4j/ObjectTest.java
+++ lib/src/test/java/io/github/mangila/ensure4j/ObjectTest.java
@@ -14,26 +14,26 @@ public class ObjectTest {
     @Test
     @DisplayName("Happy path equals(Object)")
     void equals() {
-        assertThatCode(() -> Ensure.equals(new BigDecimal("1"), new BigDecimal("1"))).doesNotThrowAnyException();
-        assertThatCode(() -> Ensure.equals(1, 1, "test message")).doesNotThrowAnyException();
-        assertThatCode(() -> Ensure.equals(1, 1, () -> new IllegalArgumentException("test message"))).doesNotThrowAnyException();
+        assertThatCode(() -> Ensure.isEquals(new BigDecimal("1"), new BigDecimal("1"))).doesNotThrowAnyException();
+        assertThatCode(() -> Ensure.isEquals(1, 1, "test message")).doesNotThrowAnyException();
+        assertThatCode(() -> Ensure.isEquals(1, 1, () -> new IllegalArgumentException("test message"))).doesNotThrowAnyException();
         // REMINDME: idk... ok?
-        assertThatCode(() -> Ensure.equals(null, null)).doesNotThrowAnyException();
+        assertThatCode(() -> Ensure.isEquals(null, null)).doesNotThrowAnyException();
     }
 
     @Test
     @DisplayName("Sad path equals(Object)")
     void equals1() {
-        assertThatThrownBy(() -> Ensure.equals(new BigDecimal("1"), new BigInteger("1")))
+        assertThatThrownBy(() -> Ensure.isEquals(new BigDecimal("1"), new BigInteger("1")))
                 .isInstanceOf(EnsureException.class)
                 .hasMessage("objects must be equal");
-        assertThatThrownBy(() -> Ensure.equals(new BigDecimal("1"), new BigInteger("1"), "test objects not equal"))
+        assertThatThrownBy(() -> Ensure.isEquals(new BigDecimal("1"), new BigInteger("1"), "test objects not equal"))
                 .isInstanceOf(EnsureException.class)
                 .hasMessage("test objects not equal");
-        assertThatThrownBy(() -> Ensure.equals(new BigDecimal("1"), new BigInteger("1"), () -> new IllegalArgumentException("test objects not equal")))
+        assertThatThrownBy(() -> Ensure.isEquals(new BigDecimal("1"), new BigInteger("1"), () -> new IllegalArgumentException("test objects not equal")))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("test objects not equal");
-        assertThatThrownBy(() -> Ensure.equals(null, new BigInteger("1"), () -> new IllegalArgumentException("test objects not equal")))
+        assertThatThrownBy(() -> Ensure.isEquals(null, new BigInteger("1"), () -> new IllegalArgumentException("test objects not equal")))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("test objects not equal");
     }
