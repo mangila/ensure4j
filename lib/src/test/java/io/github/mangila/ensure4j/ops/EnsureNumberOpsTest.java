0aaf630b099fcf831e3ab622c5e74a1c5624c757
Erik Olsson <84154996+mangila@users.noreply.github.com>
2026-02-17 18:02:54 +0100

    add method positiveWithZero (#49)

--- lib/src/test/java/io/github/mangila/ensure4j/ops/EnsureNumberOpsTest.java
+++ lib/src/test/java/io/github/mangila/ensure4j/ops/EnsureNumberOpsTest.java
@@ -109,4 +109,30 @@ public class EnsureNumberOpsTest {
                 .isInstanceOf(EnsureException.class)
                 .hasMessage("value must be negative - (0)");
     }
+
+    @Test
+    void positiveWithZeroIntSuccess() {
+        assertThat(ops.positiveWithZero(0)).isEqualTo(0);
+        assertThat(ops.positiveWithZero(1)).isEqualTo(1);
+    }
+
+    @Test
+    void positiveWithZeroIntFailure() {
+        assertThatThrownBy(() -> ops.positiveWithZero(-1))
+                .isInstanceOf(EnsureException.class)
+                .hasMessage("value must be positive or zero - (-1)");
+    }
+
+    @Test
+    void positiveWithZeroLongSuccess() {
+        assertThat(ops.positiveWithZero(0L)).isEqualTo(0L);
+        assertThat(ops.positiveWithZero(1L)).isEqualTo(1L);
+    }
+
+    @Test
+    void positiveWithZeroLongFailure() {
+        assertThatThrownBy(() -> ops.positiveWithZero(-1L))
+                .isInstanceOf(EnsureException.class)
+                .hasMessage("value must be positive or zero - (-1)");
+    }
 }
