a1ab943a639f1b203ee7b4d58527b10ddd3b7c4e
Erik Olsson <olsson.erik1993@gmail.com>
2026-05-16 16:17:53 +0200

    chore(examples): rename overloaded `placeOrder` methods for clarity in README examples
    Signed-off-by: Erik Olsson <olsson.erik1993@gmail.com>

--- examples/src/main/java/io/github/com/ensure4j/examples/ExamplesInReadMe.java
+++ examples/src/main/java/io/github/com/ensure4j/examples/ExamplesInReadMe.java
@@ -7,13 +7,13 @@ import java.util.List;
 
 final class ExamplesInReadMe {
 
-    public void placeOrder(Order order) {
+    public void placeOrder1(Order order) {
         Ensure.notNull(order, "Order cannot be null");
         Ensure.positive(order.getAmount(), "Order amount must be positive");
         // do business logic
     }
 
-    public void placeOrder(Order order) {
+    public void placeOrder2(Order order) {
         Ensure.notNull(order, () -> new OrderException("Order cannot be null"));
         Ensure.positive(order.getAmount(), () -> new OrderException("Order amount must be positive"));
         Ensure.notEmpty(order.getItems(), () -> new OrderException("Order items cannot be empty"));
@@ -21,7 +21,7 @@ final class ExamplesInReadMe {
     }
 
     // throws EnsureException with message:
-    public void placeOrder(Order order) {
+    public void placeOrder3(Order order) {
         Ensure.notNull(order); // object must not be null
         Ensure.positive(order.getAmount()); // value must be positive - %s
         Ensure.notEmpty(order.getItems()); // collection must not be empty or null
