92fbd10ebe6d1e8ac3a309224d429009a8d9ee82
Erik Olsson <olsson.erik1993@gmail.com>
2026-05-17 20:36:20 +0200

    chore(lib): update Javadoc for `EnsureException` and revert project version to 4.0.1-SNAPSHOT
    Signed-off-by: Erik Olsson <olsson.erik1993@gmail.com>

--- lib/src/main/java/io/github/mangila/ensure4j/EnsureException.java
+++ lib/src/main/java/io/github/mangila/ensure4j/EnsureException.java
@@ -2,15 +2,26 @@ package io.github.mangila.ensure4j;
 
 import java.io.Serial;
 
-/** Exception thrown by Ensure methods. */
+/** Exception thrown when a condition checked by Ensure methods is not met. */
 public class EnsureException extends RuntimeException {
 
   @Serial private static final long serialVersionUID = 1L;
 
+  /**
+   * Constructs a new EnsureException with the specified detail message.
+   *
+   * @param message the detail message.
+   */
   public EnsureException(String message) {
     super(message);
   }
 
+  /**
+   * Creates a new EnsureException with the specified detail message.
+   *
+   * @param message the detail message.
+   * @return a new EnsureException.
+   */
   public static EnsureException from(String message) {
     return new EnsureException(message);
   }
