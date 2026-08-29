263f9b24d4118cfea29ddc1c5c5b15469196ed27
Erik Olsson <olsson.erik1993@gmail.com>
2026-08-29 13:32:24 +0200

    chore(lib): fix typos in license headers
    - Corrected email domain in license headers across all source and test files (`.cm` to `.com`).
    - Ensured consistent license accuracy in all project files, including the `LICENSE`.
    
    Signed-off-by: Erik Olsson <olsson.erik1993@gmail.com>

--- lib/src/main/java/io/github/mangila/ensure4j/EnsureStringOps.java
+++ lib/src/main/java/io/github/mangila/ensure4j/EnsureStringOps.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2025 Erik Olsson (olsson.erik1993@gmail.cm)
+ * Copyright (c) 2025 Erik Olsson (olsson.erik1993@gmail.com)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
